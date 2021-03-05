80beffbcadd950944aad1fe26d094a3efeb4baf2
vardanttn <vardan.balyan@tothenew.com>
2021-03-05 12:28:34 +0530

    initial commit

---
+++ src/main/java/com/ttn/RESTFulDemo/RestFulDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ttn.RESTFulDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFulDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFulDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ttn/RESTFulDemo/RestFulDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttn.RESTFulDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFulDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
