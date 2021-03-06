6eb0028883049e7f2895a65646c7251de695dfa4
vardanttn <vardan.balyan@tothenew.com>
2021-03-06 15:32:06 +0530

    added comments

--- src/main/java/com/ttn/RESTFulDemo/employee/EmployeeNotFoundException.java
+++ src/main/java/com/ttn/RESTFulDemo/employee/EmployeeNotFoundException.java
@@ -3,6 +3,8 @@ package com.ttn.RESTFulDemo.employee;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/*
+* Using the @ResponseStatus annotation to implement NOT Found exception*/
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class EmployeeNotFoundException extends RuntimeException{
     public EmployeeNotFoundException(String message) {
